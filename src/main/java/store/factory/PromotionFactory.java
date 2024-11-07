c0f3e2dbf42988c0f5b6895294cf000d02a70826
rladmstn <oz33177351@gmail.com>
2024-11-08 01:28:39 +0900

    feat(factory) : 객체 생성하는 factory 클래스 생성
    - PromotionFactory : Promotion 객체 생성
    - ProductFactory : 프로모션 존재 여부에 따른 Product 객체 생성

---
+++ src/main/java/store/factory/PromotionFactory.java
@@ -0,0 +1,16 @@
+package store.factory;
+
+import java.time.LocalDate;
+import store.domain.Promotion;
+
+public class PromotionFactory {
+    public Promotion createPromotion(String[] input) {
+        String name = input[0];
+        int buyCount = Integer.parseInt(input[1]);
+        int getCount = Integer.parseInt(input[2]);
+        LocalDate startDate = LocalDate.parse(input[3]);
+        LocalDate endDate = LocalDate.parse(input[4]);
+
+        return new Promotion(name, buyCount, getCount, startDate, endDate);
+    }
+}
