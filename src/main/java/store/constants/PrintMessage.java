a5b50e075071068c97bd810bd71cee4452c3073a
rladmstn <oz33177351@gmail.com>
2024-11-07 21:15:29 +0900

    refactor(message) : PrintMessage enum 이름 수정
    - INPUT 관련 enum 값 이름 수정

--- src/main/java/store/constants/PrintMessage.java
+++ src/main/java/store/constants/PrintMessage.java
@@ -2,11 +2,11 @@ package store.constants;
 
 public enum PrintMessage {
     // input
-    INPUT_PRODUCT_SELECTION("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])"),
-    INPUT_PROMOTION_NOT_APPLIED("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)"),
-    INPUT_CAN_APPLY_PROMOTION("현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"),
-    INPUT_MEMBERSHIP_DISCOUNT("멤버십 할인을 받으시겠습니까? (Y/N)"),
-    INPUT_BUY_MORE("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)"),
+    INPUT_PRODUCT_SELECTION("\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])"),
+    INPUT_PROMOTION_NOT_APPLIED("\n현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)"),
+    INPUT_ADDITIONAL_PROMOTION("\n현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"),
+    INPUT_MEMBERSHIP_DISCOUNT("\n멤버십 할인을 받으시겠습니까? (Y/N)"),
+    INPUT_ADDITIONAL_PURCHASE("\n감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)"),
 
     // output
     PRINT_START("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n"),
