39b740cf148be8c06d11e7ebb8d1f8f0ec2f6d41
rladmstn <oz33177351@gmail.com>
2024-11-07 22:43:45 +0900

    refactor(reader) : 파일 읽어올 때 에러 메세지 수정

--- src/main/java/store/constants/ErrorMessage.java
+++ src/main/java/store/constants/ErrorMessage.java
@@ -5,7 +5,9 @@ public enum ErrorMessage {
     INCORRECT_PURCHASE_FORM("올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
     NOT_EXIST_PRODUCT("존재하지 않는 상품입니다. 다시 입력해 주세요."),
     OVER_STOCK("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
-    INVALID_INPUT("잘못된 입력입니다. 다시 입력해 주세요.");
+    INVALID_INPUT("잘못된 입력입니다. 다시 입력해 주세요."),
+
+    FAILED_TO_READ_FILE("파일을 읽어오는데 실패했습니다. 다시 시도해주세요.");
 
     private final String message;
 
--- src/main/java/store/util/Reader.java
+++ src/main/java/store/util/Reader.java
@@ -5,20 +5,23 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import store.constants.ErrorMessage;
 
 public final class Reader {
-    public static List<String> readFile(String filePath) throws IOException {
-        List<String> content = new ArrayList<>();
-
+    public static List<String> readFile(String filePath) {
         try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            List<String> content = new ArrayList<>();
             reader.readLine();
 
             String read;
             while ((read = reader.readLine()) != null) {
                 content.add(read);
             }
-        }
 
-        return content;
+            return content;
+        } catch (IOException e){
+            System.out.println(ErrorMessage.FAILED_TO_READ_FILE.getMessage());
+            return readFile(filePath);
+        }
     }
 }
