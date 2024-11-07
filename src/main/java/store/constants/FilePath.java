0b1683ada42406df29e270f31f83ca590eb6468c
rladmstn <oz33177351@gmail.com>
2024-11-07 19:30:58 +0900

    feat(constant) : 파일 경로 상수화 저장
    - FilePath 클래스에 정보 읽어올 파일 경로 상수로 저장
    - 디렉토리명 enums -> constants 수정

---
+++ src/main/java/store/constants/FilePath.java
@@ -0,0 +1,8 @@
+package store.constants;
+
+public final class FilePath {
+    public static final String PRODUCTS = "src/main/resources/products.md";
+    public static final String PROMOTIONS = "src/main/resources/promotions.md";
+
+    private FilePath(){}
+}
--- src/main/java/store/constants/PrintMessage.java
+++ src/main/java/store/constants/PrintMessage.java
@@ -1,4 +1,4 @@
-package store.enums;
+package store.constants;
 
 public enum PrintMessage {
     // input
