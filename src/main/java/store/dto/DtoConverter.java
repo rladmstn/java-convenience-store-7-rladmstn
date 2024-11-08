235663fc3fb9d5cc07254d69de36468a632d1c24
rladmstn <oz33177351@gmail.com>
2024-11-08 19:08:11 +0900

    feat(dto) : PurchaseInputRequest 생성하는 converter 메소드 작성
    - input String에서 구매 목록 Request DTO로 변환하는 메소드 작성

--- src/main/java/store/dto/DtoConverter.java
+++ src/main/java/store/dto/DtoConverter.java
@@ -3,6 +3,7 @@ package store.dto;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import store.constants.CommonConstant;
 import store.domain.Catalog;
 import store.domain.NormalProduct;
 import store.domain.Product;
@@ -15,16 +16,27 @@ public final class DtoConverter {
         List<ProductResponse> response = new ArrayList<>();
         Map<String, List<Product>> list = catalog.getCatalog();
 
-        for(String name : list.keySet()){
-            for(Product product : list.get(name)){
+        for (String name : list.keySet()) {
+            for (Product product : list.get(name)) {
                 response.add(generateProductResponse(product));
             }
         }
         return response;
     }
 
+    public static List<PurchaseInputRequest> toPurchaseInputRequest(String input) {
+        List<PurchaseInputRequest> requests = new ArrayList<>();
+        String[] purchases = input.split(CommonConstant.DELIMITER.get());
+        for (String purchase : purchases) {
+            String[] tempInput = purchase.substring(1, purchase.length() - 1)
+                    .split(CommonConstant.PURCHASE_DELIMITER.get());
+            requests.add(new PurchaseInputRequest(tempInput[0], Integer.parseInt(tempInput[1])));
+        }
+        return requests;
+    }
+
     private static ProductResponse generateProductResponse(Product product) {
-        if(product instanceof NormalProduct){
+        if (product instanceof NormalProduct) {
             return new ProductResponse(product.getName(), product.getPrice(), product.getStockToString(), EMPTY_STRING);
         }
         PromotionProduct promotionProduct = (PromotionProduct) product;
@@ -34,5 +46,4 @@ public final class DtoConverter {
                 promotionProduct.getStockToString(),
                 promotionProduct.getPromotionName());
     }
-
 }
