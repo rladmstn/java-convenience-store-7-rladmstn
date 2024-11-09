55303ed5072d214b140f77c43b050d55d99a813a
rladmstn <oz33177351@gmail.com>
2024-11-09 11:20:00 +0900

    refactor(promotion) : 프로모션 필드명 수정
    - Promotion 필드에서 증정용 개수를 getCount -> giveCount로 수정

--- src/main/java/store/domain/Promotion.java
+++ src/main/java/store/domain/Promotion.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public record Promotion(String name,
                         int buyCount,
-                        int getCount,
+                        int giveCount,
                         LocalDate startDate,
                         LocalDate endDate) {
 
--- src/main/java/store/factory/PromotionFactory.java
+++ src/main/java/store/factory/PromotionFactory.java
@@ -7,10 +7,10 @@ public class PromotionFactory {
     public Promotion createPromotion(String[] input) {
         String name = input[0];
         int buyCount = Integer.parseInt(input[1]);
-        int getCount = Integer.parseInt(input[2]);
+        int giveCount = Integer.parseInt(input[2]);
         LocalDate startDate = LocalDate.parse(input[3]);
         LocalDate endDate = LocalDate.parse(input[4]);
 
-        return new Promotion(name, buyCount, getCount, startDate, endDate);
+        return new Promotion(name, buyCount, giveCount, startDate, endDate);
     }
 }
