2d1186d4184a63ce18f8456c589fa72615ce9d19
rladmstn <oz33177351@gmail.com>
2024-11-10 21:09:09 +0900

    feat(controller) : Controller에 입출력 관련 메소드 작성
    - 카탈로그 보여주고 구매 입력 받는 메소드 작성
    - 프로모션 증정 1개 받을지 여부 입력 받는 메소드 작성
    - 원가 구매를 진행할지 여부 입력 받는 메소드 작성
    - 멤버십 할인 적용할지 여부 입력 받는 메소드 작성

--- src/main/java/store/controller/ConvenienceStoreController.java
+++ src/main/java/store/controller/ConvenienceStoreController.java
@@ -1,22 +1,73 @@
 package store.controller;
 
 import java.util.List;
+import store.constants.CommonConstant;
 import store.domain.Catalog;
 import store.domain.Promotion;
+import store.dto.DtoConverter;
+import store.dto.PurchaseInputRequest;
+import store.service.ConvenienceStoreService;
+import store.util.InputValidator;
 import store.view.InputView;
 import store.view.OutputView;
 
 public class ConvenienceStoreController {
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
-    private final List<Promotion> promotions;
+    private final ConvenienceStoreService service;
     private final Catalog catalog;
+    private final List<Promotion> promotions;
 
-    public ConvenienceStoreController(List<Promotion> promotions,Catalog catalog) {
-        this.promotions = promotions;
+    public ConvenienceStoreController(Catalog catalog, List<Promotion> promotions) {
         this.catalog = catalog;
+        this.service = new ConvenienceStoreService(catalog);
+        this.promotions = promotions;
     }
 
-    public void run(){
+    public void run() {
+    }
+
+    private List<PurchaseInputRequest> getPurchaseSelection() {
+        try {
+            String input = inputView.inputProductSelection();
+            InputValidator.validatePurchaseSelection(input, catalog);
+            return DtoConverter.toPurchaseInputRequest(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getPurchaseSelection();
+        }
+    }
+
+    private boolean wantsToAddPromotionProduct(String productName) {
+        try {
+            String input = inputView.askAdditionalPromotion(productName);
+            InputValidator.validateAnswer(input);
+            return input.equals(CommonConstant.YES.get());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return wantsToAddPromotionProduct(productName);
+        }
+    }
+
+    private boolean wantsToOriginalPurchase(String productName, int count) {
+        try {
+            String input = inputView.askNonPromotionPurchase(productName, count);
+            InputValidator.validateAnswer(input);
+            return input.equals(CommonConstant.YES.get());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return wantsToOriginalPurchase(productName, count);
+        }
+    }
+
+    private boolean wantsToApplyMembershipDiscount() {
+        try {
+            String input = inputView.askMembershipDiscount();
+            InputValidator.validateAnswer(input);
+            return input.equals(CommonConstant.YES.get());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return wantsToApplyMembershipDiscount();
+        }
     }
 }
