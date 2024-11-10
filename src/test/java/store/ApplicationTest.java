24acd79019f8d10684ce518e8a7d922be4da3089
rladmstn <oz33177351@gmail.com>
2024-11-11 02:13:52 +0900

    test(application) : 예외 관련 테스트 케이스 추가
    - input 검증 실패 시 발생할 수 있는 예외 메세지 테스트 추가

--- src/test/java/store/ApplicationTest.java
+++ src/test/java/store/ApplicationTest.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertNowTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -15,24 +17,24 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() -> {
             run("[물-1]", "N", "N");
             assertThat(output()).contains(
-                "- 콜라 1,000원 10개 탄산2+1",
-                "- 콜라 1,000원 10개",
-                "- 사이다 1,000원 8개 탄산2+1",
-                "- 사이다 1,000원 7개",
-                "- 오렌지주스 1,800원 9개 MD추천상품",
-                "- 오렌지주스 1,800원 재고 없음",
-                "- 탄산수 1,200원 5개 탄산2+1",
-                "- 탄산수 1,200원 재고 없음",
-                "- 물 500원 10개",
-                "- 비타민워터 1,500원 6개",
-                "- 감자칩 1,500원 5개 반짝할인",
-                "- 감자칩 1,500원 5개",
-                "- 초코바 1,200원 5개 MD추천상품",
-                "- 초코바 1,200원 5개",
-                "- 에너지바 2,000원 5개",
-                "- 정식도시락 6,400원 8개",
-                "- 컵라면 1,700원 1개 MD추천상품",
-                "- 컵라면 1,700원 10개"
+                    "- 콜라 1,000원 10개 탄산2+1",
+                    "- 콜라 1,000원 10개",
+                    "- 사이다 1,000원 8개 탄산2+1",
+                    "- 사이다 1,000원 7개",
+                    "- 오렌지주스 1,800원 9개 MD추천상품",
+                    "- 오렌지주스 1,800원 재고 없음",
+                    "- 탄산수 1,200원 5개 탄산2+1",
+                    "- 탄산수 1,200원 재고 없음",
+                    "- 물 500원 10개",
+                    "- 비타민워터 1,500원 6개",
+                    "- 감자칩 1,500원 5개 반짝할인",
+                    "- 감자칩 1,500원 5개",
+                    "- 초코바 1,200원 5개 MD추천상품",
+                    "- 초코바 1,200원 5개",
+                    "- 에너지바 2,000원 5개",
+                    "- 정식도시락 6,400원 8개",
+                    "- 컵라면 1,700원 1개 MD추천상품",
+                    "- 컵라면 1,700원 10개"
             );
         });
     }
@@ -46,10 +48,10 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 여러_개의_프로모션_상품_구매(){
+    void 여러_개의_프로모션_상품_구매() {
         assertSimpleTest(() -> {
-            run("[콜라-5]","Y","N","N");
-            assertThat(output().replaceAll("\\s","")).contains("내실돈4,000");
+            run("[콜라-5]", "Y", "N", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈4,000");
         });
     }
 
@@ -62,34 +64,40 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 멤버십_할인_적용(){
+    void 멤버십_할인_적용() {
         assertSimpleTest(() -> {
-            run("[물-2]","Y","N");
-            assertThat(output().replaceAll("\\s","")).contains("내실돈700");
+            run("[물-2]", "Y", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈700");
         });
     }
 
     @Test
-    void 원가_구매가_필요한_경우_원가_구매(){
+    void 원가_구매가_필요한_경우_원가_구매() {
         assertSimpleTest(() -> {
-           run("[콜라-12]","Y","N","N");
-           assertThat(output().replaceAll("\\s","")).contains("내실돈9,000");
+            run("[콜라-12]", "Y", "N", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈9,000");
         });
     }
 
     @Test
-    void 원가_구매가_필요한_경우_원가_구매_안함(){
+    void 원가_구매가_필요한_경우_원가_구매_안함() {
         assertSimpleTest(() -> {
-            run("[콜라-12]","N","N","N");
-            assertThat(output().replaceAll("\\s","")).contains("내실돈6,000");
+            run("[콜라-12]", "N", "N", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈6,000");
         });
     }
 
-    @Test
-    void 예외_테스트() {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "[콜라-12]/A/N/[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.",
+            "[컵라면-12]/N/N/[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.",
+            "[abc123]/Y/Y/[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.",
+            "[청포도에이드-3]/Y/Y/[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요."
+    }, delimiter = '/')
+    void 예외_테스트(String purchase, String answer1, String answer2, String expected) {
         assertSimpleTest(() -> {
-            runException("[컵라면-12]", "N", "N");
-            assertThat(output()).contains("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+            runException(purchase, answer1, answer2);
+            assertThat(output()).contains(expected);
         });
     }
 
