9aea9552aeecfb9e251d065548edfc69c5ca4cd5
rladmstn <oz33177351@gmail.com>
2024-11-10 23:54:38 +0900

    feat(dto) : 구매 결과 목록을 영수증 DTO로 변환하는 메소드 작성

--- src/main/java/store/dto/DtoConverter.java
+++ src/main/java/store/dto/DtoConverter.java
@@ -4,13 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import store.constants.CommonConstant;
+import store.constants.ReceiptCategory;
 import store.domain.Catalog;
 import store.domain.NormalProduct;
 import store.domain.Product;
 import store.domain.PromotionProduct;
+import store.domain.PromotionPurchaseResult;
+import store.domain.PurchaseResult;
+import store.util.NumberFormatUtil;
 
 public final class DtoConverter {
     private static final String EMPTY_STRING = "";
+    private static final String MINUS = "-";
 
     public static List<ProductResponse> toCatalogResponse(Catalog catalog) {
         List<ProductResponse> response = new ArrayList<>();
@@ -33,6 +38,59 @@ public final class DtoConverter {
             requests.add(new PurchaseInputRequest(tempInput[0], Integer.parseInt(tempInput[1])));
         }
         return requests;
+    }
+
+    public static ReceiptResponse toReceiptResponse(List<PurchaseResult> purchaseResults, int membershipDiscount) {
+        int finalAmount = getFinalAmount(purchaseResults);
+        int finalCount = getFinalCount(purchaseResults);
+        int finalPromotionAmount = getFinalPromotionAmount(purchaseResults);
+        List<PurchasedProductResponse> allPurchasedResponses = new ArrayList<>();
+        List<PromotionProductResponse> promotionsResponses = new ArrayList<>();
+        addResultToResponses(purchaseResults, allPurchasedResponses, promotionsResponses);
+        TotalPaymentResponse totalPaymentResponse = new TotalPaymentResponse(finalCount, finalAmount);
+        List<SummaryResponse> summaryResponse = getSummaryResponses(membershipDiscount, finalPromotionAmount,
+                finalAmount);
+        return new ReceiptResponse(allPurchasedResponses, promotionsResponses, totalPaymentResponse, summaryResponse);
+    }
+
+    private static int getFinalCount(List<PurchaseResult> results) {
+        return results.stream().mapToInt(PurchaseResult::getTotalCount).sum();
+    }
+
+    private static int getFinalAmount(List<PurchaseResult> results) {
+        return results.stream().mapToInt(PurchaseResult::getTotalAmount).sum();
+    }
+
+    private static int getFinalPromotionAmount(List<PurchaseResult> results) {
+        return results.stream().filter(result -> result instanceof PromotionPurchaseResult)
+                .mapToInt(result -> ((PromotionPurchaseResult) result).getPromotionAppliedAmount())
+                .sum();
+    }
+
+    private static void addResultToResponses(List<PurchaseResult> purchaseResults,
+                                             List<PurchasedProductResponse> purchasedProductResponses,
+                                             List<PromotionProductResponse> promotionProductResponses) {
+        for (PurchaseResult purchaseResult : purchaseResults) {
+            purchasedProductResponses.add(
+                    new PurchasedProductResponse(purchaseResult.getName(), purchaseResult.getTotalCount(),
+                            purchaseResult.getTotalAmount()));
+            if (purchaseResult instanceof PromotionPurchaseResult) {
+                promotionProductResponses.add(new PromotionProductResponse(purchaseResult.getName(),
+                        ((PromotionPurchaseResult) purchaseResult).getPromotionAppliedCount()));
+            }
+        }
+    }
+
+    private static List<SummaryResponse> getSummaryResponses(int membershipDiscount, int finalPromotionAmount,
+                                                             int finalAmount) {
+        SummaryResponse promotionResponse = new SummaryResponse(ReceiptCategory.PROMOTION_CATEGORY.get(),
+                MINUS + NumberFormatUtil.numberFormat(finalPromotionAmount));
+        SummaryResponse membershipResponse = new SummaryResponse(ReceiptCategory.MEMBERSHIP_CATEGORY.get(),
+                MINUS + NumberFormatUtil.numberFormat(membershipDiscount));
+        SummaryResponse finalPaymentResponse = new SummaryResponse(ReceiptCategory.FINAL_PAYMENT_CATEGORY.get(),
+                String.valueOf(finalAmount - finalPromotionAmount - membershipDiscount));
+
+        return List.of(promotionResponse, membershipResponse, finalPaymentResponse);
     }
 
     private static ProductResponse generateProductResponse(Product product) {
